135c80d0d4f577077d5bc8d50458104f9e316fa8
kouta1612 <kouta1612world69@gmail.com>
2020-02-09 20:36:16 +0900

    add signature

--- app/Http/Controllers/Api/UploadController.php
+++ app/Http/Controllers/Api/UploadController.php
@@ -8,11 +8,164 @@ use Illuminate\Http\Request;
 
 class UploadController extends Controller
 {
+    private $request;
+    private $fileKey;
+    private $now;
+    private $bucket;
+    private $accessKey;
+    private $region;
+    private $secret;
+    private $url;
+    private $policy;
+    private $dateKey;
+    private $dateRegionKey;
+    private $dateRegionServiceKey;
+    private $signingKey;
+    private $signature;
+    private $data;
+
+    public function policies(Request $request)
+    {
+        $this->setRequest($request);
+        $this->setNow(time());
+        // AWS設定
+        $this->setFileKey('test');
+        $this->setBucket(config('filesystems.disks.s3.bucket'));
+        $this->setAccessKey(config('filesystems.disks.s3.key'));
+        $this->setRegion(config('filesystems.disks.s3.region'));
+        $this->setSecret(config('filesystems.disks.s3.secret'));
+        $this->setUrl('https://' . $this->bucket . '.s3.amazonaws.com');
+        // ポリシー作成
+        $this->setPolicy();
+        // 署名作成
+        $this->setDateKey();
+        $this->setDateRegionKey();
+        $this->setDateRegionServiceKey();
+        $this->setSigningKey();
+        // シグネチャ作成
+        $this->setSignature();
+        // POSTデータ作成
+        $this->setData();
+
+        return [
+            'url' => $this->url,
+            'date' => $this->data
+        ];
+    }
+
     public function upload(Request $request)
     {
+        // S3にファイルをアップロード
         $upload = new Upload('test', $request->file);
         $upload->saveFileToS3();
 
         return ['url' => $upload->getFilePath()];
+    }
+
+    private function setRequest($request)
+    {
+        $this->request = $request;
+    }
+
+    private function setFileKey($fileKey)
+    {
+        $this->fileKey = $fileKey;
+    }
+
+    private function setNow($now)
+    {
+        $this->now = $now;
+    }
+
+    private function setBucket($bucket)
+    {
+        $this->bucket = $bucket;
+    }
+
+    private function setAccessKey($accessKey)
+    {
+        $this->accessKey = $accessKey;
+    }
+
+    private function setRegion($region)
+    {
+        $this->region = $region;
+    }
+
+    private function setSecret($secret)
+    {
+        $this->secret = $secret;
+    }
+
+    private function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    private function setPolicy()
+    {
+        $policyDocument = [
+            'expiration' => gmdate('Y-m-d\TH:i:s.000\Z', $this->now + 60),
+            'conditions' => [
+                ['bucket' => config('filesystems.disks.s3.bucket')],
+                ['key' => $this->fileKey],
+                ['Content-Type' => $this->request->contentType],
+                ['content-length-range', $this->request->size, $this->request->size],
+                ['acl' => 'public-read'],
+                ['success_action_status' => '201'],
+                ['x-amz-algorithm' => 'AWS4-HMAC-SHA256'],
+                ['x-amz-credential' => implode('/', [config('filesystems.disks.s3.key'), gmdate('Ymd', $this->now), config('filesystems.disks.s3.region'), 's3', 'aws4_request'])],
+                ['x-amz-date' => gmdate('Ymd\THis\Z', $this->now)]
+            ]
+        ];
+        $this->policy = base64_encode(json_encode($policyDocument));
+    }
+
+    private function setDateKey()
+    {
+        $this->dateKey = hash_hmac(
+            'sha256',
+            gmdate('Ymd', $this->now),
+            'AWS4' . $this->secret,
+            true
+        );
+    }
+
+    private function setDateRegionKey()
+    {
+        $this->dateRegionKey = hash_hmac('sha256', $this->region, $this->dateKey, true);
+    }
+
+    private function setDateRegionServiceKey()
+    {
+        $this->dateRegionServiceKey = hash_hmac('sha256', 's3', $this->dateRegionKey, true);
+    }
+
+    private function setSigningKey()
+    {
+        $this->signingKey = hash_hmac('sha256', 'aws4_request', $this->dateRegionServiceKey, true);
+    }
+
+    private function setSignature()
+    {
+        $this->signature = hash_hmac('sha256', $this->policy, $this->signingKey, false);
+    }
+
+    private function setData()
+    {
+        $this->data = [
+            'bucket' => $this->bucket,
+            'key' => $this->fileKey,
+            'Content-Type' => $this->request->type,
+            'acl' => 'public-read',
+            'success_action_status' => 201,
+            'policy' => $this->policy,
+            'x-amz-credential' => implode('/', [
+                $this->accessKey, gmdate('Ymd', $this->now), $this->region, 's3', 'aws4_request'
+            ]),
+            'x-amz-signature' => $this->signature,
+            'x-amz-algorithm' => 'AWS4-HMAC-SHA256',
+            'x-amz-date' => gmdate('Ymd\THis\Z', $this->now)
+        ];
     }
 }
--- resources/js/upload.js
+++ resources/js/upload.js
@@ -1,16 +1,38 @@
 let fileInput = $('#file');
 fileInput.on('change', (e) => {
-    let formdata = new FormData();
-    formdata.append('file', e.target.files[0]);
+    let file = e.target.files[0];
+    $.ajax({
+        url: '/api/signature',
+        type: 'POST',
+        data: {
+            size: file.size,
+            type: file.type
+        },
+        dataType: 'json'
+    }).then((response) => {
+        let key, formData = new FormData();
+        for (key in response.data) {
+            if (response.data.hasOwnProperty(key)) {
+                formdata.append(key, response.data[key]);
+            }
+        }
+        formdata.append('file', file);
 
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', '/api/upload', true);
-    xhr.send(formdata);
-    xhr.onload = () => {
-        console.log(JSON.parse(xhr.response).url);
+        return $.ajax({
+            url: response.url,
+            type: 'POST',
+            data: formData,
+            dataType: 'xml',
+            processData: false,
+            contentType: false
+        }).catch((error) => {
+            console.log('署名作成エラー');
+            console.log(error);
+        });
+    }).then((response) => {
         let body = $('body');
         let img = document.createElement('img');
-        img.src = JSON.parse(xhr.response).url;
+        img.src = $(response).find('Location').first().text();
         body.append(img);
-    }
+    })
 });
