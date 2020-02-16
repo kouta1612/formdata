62353706964cf0b23ae3d7ee2d9285b9c699c9fb
kouta1612 <kouta1612world69@gmail.com>
2020-02-17 08:53:19 +0900

    fix canvas bug

--- resources/js/upload.js
+++ resources/js/upload.js
@@ -8,7 +8,7 @@ document.getElementById('file').addEventListener('change', (e) => {
 async function handle(image) {
     // シグネチャ作成とリサイズを並列処理
     const [data, resizedImage] = await Promise.all(
-        [signature(image), resize(image)]
+        [signature(image), resize(image, 500)]
     );
     // シグネチャとリサイズしたファイルをs3にわたす
     const uploadedImage = await upload(data, resizedImage);
@@ -29,34 +29,40 @@ function signature(image) {
     });
 }
 
-async function resize(image) {
-    const newImage = await readDataUrl(image);
-    return new Promise(resolve => {
-        newImage.onload = async () => {
-            let canvas = await drawCanvas(newImage);
-            let blob = await canvasToBlob(canvas, newImage);
-            resolve(blob);
-        };
-    });
-}
-
-function readDataUrl(image) {
+async function resize(image, max) {
     return new Promise(resolve => {
         const reader = new FileReader();
-        let newImage = new Image();
         reader.onload = (e) => {
+            let newImage = new Image();
+            newImage.onload = async () => {
+                let [width, height] = await ratio(newImage, max);
+                let canvas = await drawCanvas(newImage, width, height);
+                let blob = await canvasToBlob(canvas, newImage);
+                resolve(blob);
+            }
             newImage.src = e.target.result;
-            resolve(newImage);
         };
         reader.readAsDataURL(image);
     });
 }
 
-function drawCanvas(image) {
+function ratio(image, max) {
+    if (image.width < max && image.height < max) {
+        return [image.width, image.height];
+    } else {
+        if (image.width > image.height) {
+            return [max, (image.height * max) / image.width];
+        } else {
+            return [max, (image.width * max) / image.height];
+        }
+    }
+}
+
+function drawCanvas(image, width, height) {
     let canvas = document.createElement('canvas');
     let ctx = canvas.getContext('2d');
-    [canvas.width, canvas.height] = [image.width, image.height];
-    ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, image.width * 0.1, image.height * 0.1);
+    [canvas.width, canvas.height] = [width, height];
+    ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, width, height);
     return canvas;
 }
 
