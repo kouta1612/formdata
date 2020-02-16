adeec0acaeaeb913ce1e676b3fffa6e734a3a34a
kouta1612 <kouta1612world69@gmail.com>
2020-02-16 18:02:34 +0900

    add upload resize file

--- resources/js/upload.js
+++ resources/js/upload.js
@@ -1,7 +1,21 @@
-let fileInput = $('#file');
-fileInput.on('change', (e) => {
-    let file = e.target.files[0];
-    $.ajax({
+// ファイルの読み込み
+$('#file').on('change', (e) => {
+    handle(e.target.files[0]);
+});
+
+async function handle(file) {
+    // シグネチャ作成
+    const data = await signature(file);
+    // ファイルのリサイズ
+    const resizedFile = await resize(file);
+    // シグネチャとリサイズしたファイルをs3にわたす
+    const uploadedFile = await upload(data, resizedFile);
+    // s3から受け取ったファイルを表示
+    display(uploadedFile);
+}
+
+function signature(file) {
+    return $.ajax({
         url: '/api/signature',
         type: 'POST',
         data: {
@@ -9,57 +23,58 @@ fileInput.on('change', (e) => {
             size: file.size,
             type: file.type
         },
-        dataType: 'json'
-    }).then((response) => {
-        let key;
-        let formData = new FormData();
-        for (key in response.data) {
-            if (response.data.hasOwnProperty(key)) {
-                formData.append(key, response.data[key]);
+        dataType: 'json',
+    });
+}
+
+function resize(file) {
+    const reader = new FileReader();
+    let image = new Image();
+    reader.onload = (e) => {
+        image.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+
+    return new Promise(resolve => {
+        image.onload = () => {
+            let canvas = document.createElement('canvas');
+            let ctx = canvas.getContext('2d');
+            [canvas.width, canvas.height] = [image.width, image.height];
+            ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, image.width * 0.5, image.height * 0.5);
+            let base64 = canvas.toDataURL(file.type);
+            let binary = atob(base64.replace(/^.*,/, ''));
+            const buffer = new Uint8Array(binary.length);
+            for (let i = 0; i < binary.length; i++) {
+                buffer[i] = binary.charCodeAt(i);
             }
-        }
-        // リサイズ
-        const reader = new FileReader();
-        var blob;
-        reader.onload = (e) => {
-            let image = new Image();
-            image.onload = () => {
-                let canvas = document.createElement('canvas');
-                let ctx = canvas.getContext('2d');
-                [canvas.width, canvas.height] = [image.width, image.height];
-                ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, image.width * 0.5, image.height * 0.5);
-                let base64 = canvas.toDataURL(file.type);
-                let binary = atob(base64.replace(/^.*,/, ''));
-                const buffer = new Uint8Array(binary.length);
-                for (let i = 0; i < binary.length; i++) {
-                    buffer[i] = binary.charCodeAt(i);
-                }
-                blob = new Blob([buffer.buffer], { type: file.type });
-                // console.log(blob)
-            };
-            image.src = e.target.result;
+            let blob = new Blob([buffer.buffer], { type: file.type });
+            resolve(blob);
         };
-        reader.readAsDataURL(file);
-        formData.append('file', blob);
-        console.log(blob)
-        return $.ajax({
-            url: response.url,
-            type: 'POST',
-            data: formData,
-            dataType: 'xml',
-            processData: false,
-            contentType: false,
-        }).catch((error) => {
-            console.log('署名作成エラー');
-            console.log(error);
-        });
-    }).then((response) => {
-        let body = $('body');
-        let img = document.createElement('img');
-        img.src = $(response).find('Location').first().text();
-        body.append(img);
-    }).catch((error) => {
-        console.log('アップロードエラー');
-        console.log(error);
-    })
-});
+    });
+}
+
+function upload(signature, resizedFile) {
+    let key;
+    let formData = new FormData();
+    for (key in signature.data) {
+        if (signature.data.hasOwnProperty(key)) {
+            formData.append(key, signature.data[key]);
+        }
+    }
+    formData.append('file', resizedFile);
+    return $.ajax({
+        url: signature.url,
+        type: 'POST',
+        data: formData,
+        dataType: 'xml',
+        processData: false,
+        contentType: false,
+    });
+}
+
+function display(uploadedFile) {
+    let body = $('body');
+    let img = document.createElement('img');
+    img.src = $(uploadedFile).find('Location').first().text();
+    body.append(img);
+}
